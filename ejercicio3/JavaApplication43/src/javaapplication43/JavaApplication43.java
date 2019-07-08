1e3e29a1f020557d1849e70876ffbec599aab527
husney <usneibayon@gmail.com>
2019-07-08 18:06:02 -0500

    Ejercicio 3 terminado

--- ejercicio3/JavaApplication43/src/javaapplication43/JavaApplication43.java
+++ ejercicio3/JavaApplication43/src/javaapplication43/JavaApplication43.java
@@ -18,24 +18,75 @@ package javaapplication43;
 
 public class JavaApplication43 {
 
-    /*
-    x=1
-    y = 1^2  - 2(1)
-    
-    x=2
-    Y = 2^2 - 2(2)
-    
-    x=3
-    
-     */
+
     public static void main(String[] args) {
 
-        int a = 0;
-        int b = 0;
-        int y = 0;
-        int x = 0;
-
-        int suma = 0;
+        int a= 0;
+        int b= 0;
+        int y= 0;
+        int suma=0;
+        int suma3=0;
+        int suma5=0;
+        
+        
+        
+        
+        for ( int i=1; i<=10; i++){ //imprimir valores de 1 a 10
+            
+            a= (int)Math.pow(i, 2);            //x ala 2
+            b= 2*i;                         //2*x
+            y= a-b;                     //valor de Y 
+            System.out.println(+i+ " Y: " +y);
+            System.out.println("---------------------------------------------------------------------------");
+            suma+= y;                           // suma de los valores de Y 
+        }
+        
+        System.out.println("La suma de todos los valores de Y es: "+suma);
+        System.out.println("---------------------------------------------------------------------------");
+        
+        for( int i=1; i <=10;i++ ){     
+            if(i%3==0){                             //multiplos de 3
+               suma3+= i;                               // suma de los multiplos de 3
+                System.out.println( "impar:" +i );          
+            }
+        }
+        System.out.println("la suma de los multiplos de 3 es: " + suma3);
+        System.out.println("---------------------------------------------------------------------------");
+        
+        for ( int i=1; i<=10; i++){
+            
+            if(i%5==0){                             // multiplos de 5
+                suma5+=i;                                   // suma de multiplos de 5
+                System.out.println("Multiplo de 5: " + i);
+            }
+        }System.out.println("La suma de los multiplos de 5 es: " +suma5);
+        System.out.println("---------------------------------------------------------------------------");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        int a = 0;
+//        int b = 0;
+//        int y = 0;
+//        int x = 0;
+//
+//        int suma = 0;
 //
 //        for (int i = 1; i <= 10; i++) {
 //
