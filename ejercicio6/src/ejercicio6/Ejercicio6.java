73bc919aa0518ddab121b07522393204f4999ec5
husney <usneibayon@gmail.com>
2019-07-10 20:03:08 -0500

    Ejercicio 6 Sucesión Resuelto

---
+++ ejercicio6/src/ejercicio6/Ejercicio6.java
@@ -0,0 +1,50 @@
+/*
+ hacer un algoritmo que imprima los primeros 20 términos de la siguiente serie:
+
+​
+
+1, 3, 6, 10, 15, 21, 28,……..
+
+​
+ */
+package ejercicio6;
+
+
+public class Ejercicio6 {
+
+    
+    public static void main(String[] args) {
+    
+    
+      System.out.println("Hola");
+        int resultado=0;
+        for (int i=1; i<50; i++){
+            resultado= (int)(Math.pow(i, 2)+i)/2;
+        System.out.println(resultado);
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
